c9261fabb774423a25afa0f56b6601450a279fce
shenleilei <u1349682@umail.utah.edu>
2023-04-18 14:29:53 -0600

    add money/reputation system

--- iRestMyCase/model.cpp
+++ iRestMyCase/model.cpp
@@ -5,3 +5,28 @@ Model::Model(QObject *parent)
 {
 
 }
+
+Model::addMoney(int value){
+    money += value;
+}
+
+Model::deleteMoney(int value){
+    money -= value;
+    if(money <= 0){
+        //game over
+    }
+}
+
+Model::addReputation(double value){
+    reputation += value;
+    if(){
+        // Check if the reputation has reached the requirements of the next stage, and if so, promote
+    }
+}
+
+Model::deleteReputation(double value){
+    reputation -= value;
+    if(reputation <= 0){
+        //Check if there is a upper level, if not game over
+    }
+}
--- iRestMyCase/model.h
+++ iRestMyCase/model.h
@@ -7,11 +7,18 @@
 class Model : public QObject
 {
     Q_OBJECT
+private:
+    int money;
+    double reputation;
 public:
     explicit Model(QObject *parent = nullptr);
     std::map<int, Client> currentClients; //Pool of current clients
     std::map<int, Client> unusedClients; //Select from this pool of not yet used clients
     int money, reputation;
+    void addMoney(int value);
+    void deleteMoney(int value);
+    void addReputation(double value);
+    void deleteReputation(double value);
 
 signals:
 
